b4cd31947d3ad6fccfd04357c520c708c45f4ff2
trzeci <kontakt@trzeci.eu>
2015-12-18 10:59:54 +0100

    Improve size, compile master and incoming

--- generator.py
+++ generator.py
@@ -7,7 +7,6 @@ emscripten_git_repo = 'https://github.com/kripken/emscripten/'
 docker_hub_repo = "trzeci/emscripten"
 minimum_version = "1.34.1"
 
-
 def is_version_at_least(ver, target):
 	ver = ver.split('.')
 	target = target.split('.')
@@ -56,7 +55,19 @@ def get_builds(tags, b64=False):
 					"sdk": sdk,
 					"docker_tag": sdk,
 					"docker_name" : docker_hub_repo + ":" + sdk,
+					"update" : False,
 				})
+		for branch in ["master", "incoming"]:
+			sdk = "sdk-" + branch + "-" + bits
+			builds.append({
+				"tag": branch,
+				"dir": branch,
+				"sdk": sdk,
+				"docker_tag": sdk,
+				"docker_name" : docker_hub_repo + ":" + sdk,
+				"update" : True,
+			})
+
 	return builds
 
 def get_server_tags():
@@ -97,53 +108,50 @@ def rename(builds):
 			log("Already Exists: " + pb)
 			continue
 
-		log("RETAG: " + pb  + " => " + tag)
-		# get image
+		log("[INFO] RETAG: " + pb  + " => " + tag)
 		subprocess.call(["docker", "pull", docker_hub_repo + ":" + pb])
-		# re-tag
 		subprocess.call(["docker", "tag", docker_hub_repo + ":" + pb, docker_hub_repo + ":" + tag])
-		# push
 		subprocess.call(["docker", "push", docker_hub_repo + ":" + tag])
-		# clean
 		subprocess.call(["docker", "rmi", "-f", docker_hub_repo + ":" + pb])
 		subprocess.call(["docker", "rmi", "-f", docker_hub_repo + ":" + tag])
 
 
 def generate(builds, serverTags):
 	for build in builds:
-		# Check that tag exists in remote repository 
 		if build["docker_tag"] in serverTags:
-			log("[INFO] Not need to create " + build["docker_tag"])
-			continue
+			if build["update"]:
+				log("[INFO] Update tag: " + build["docker_tag"])
+				subprocess.call(["docker", "pull", build["docker_name"]])
+			else:
+				log("[INFO] Not need to create " + build["docker_tag"])
+				continue
 
 		log("[INFO] Start building {tag}".format(tag=build["docker_tag"]))
 
 		generate_dockerfile("Dockerfile", build)
 
 		# generate docker image 
-		ret = subprocess.call(["docker", "build", "-t",  build["docker_name"], "."])
-
-		if ret != 0:
+		if subprocess.call(["docker", "build", "-t",  build["docker_name"], "."]):
 			log("[ERROR] Building {tag} failed".format(tag=build["docker_tag"]))
 			continue
 
 		# test image by compiling sample.cpp
 		t_start = datetime.datetime.now()
-		ret = subprocess.call(["docker run -v $(pwd):/src " + build["docker_name"]+ " emcc test.cpp -o test.js && nodejs test.js"], shell=True)
-
-		if ret != 0:
+		if subprocess.call(["docker run -v $(pwd):/src " + build["docker_name"]+ " emcc test.cpp -o test.js && nodejs test.js"], shell=True):
 			log("[ERROR] Testing {tag} failed".format(tag=build["docker_tag"]))
 			continue
-		else:
-			log("[INFO] Compiling [{tag}] in: {time}".format(tag=build["docker_tag"], time=str(datetime.datetime.now() - t_start)))
+		log("[INFO] Compiling [{tag}] in: {time}".format(tag=build["docker_tag"], time=str(datetime.datetime.now() - t_start)))
 
 		# push to docker repository
+		pushed = False
 		for i in repeat(None, 3):
-			ret = subprocess.call(["docker", "push", build["docker_name"]])
-			if ret != 0:
+			if subprocess.call(["docker", "push", build["docker_name"]]):
 				log("[WARNING] Pushing {tag} failed. Repeat.".format(tag=build["docker_tag"]))
+			else:
+				pushed = True
+				break
 		
-		if ret != 0:
+		if not pushed:
 			log("[ERROR] Pushing {tag} failed.".format(tag=build["docker_tag"]))
 			continue
 
