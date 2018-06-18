6563679e04c1f81c1032c3979b3e1b602117b177
Piotr Paczkowski <kontakt@trzeci.eu>
2018-06-18 19:52:58 +0200

    Add update script for automation
    Please use it wisely, because it does an aggresive docker cleanup before start

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Clean unused docker images"
+docker rm $(docker ps -a -q) 2> /dev/null
+docker rmi $(docker images -q) 2> /dev/null
+
+echo "Update emscripten-docker script"
+git clean -fdx
+git pull
+
+echo "Compile and push"
+./build compile --incoming --branches --fast-fail
+./build push --no-block
+
+echo "DONE"
+
+
