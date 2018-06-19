65c591feb28ccf6fd63f375c5c5f106a25611363
Piotr.Paczkowski <piotr.paczkowski@king.com>
2018-06-19 06:11:14 +0200

    Fix CWD

--- update.sh
+++ update.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+
 echo "Clean unused docker images"
 docker rm $(docker ps -a -q) 2> /dev/null
 docker rmi $(docker images -q) 2> /dev/null
 
-echo "Update emscripten-docker script"
-git clean -fdx
-git pull
+pushd $BASEDIR
+    echo "Update emscripten-docker script"
+    git clean -fdx
+    git pull
 
-set +x
-echo "Compile and push"
-./build compile --branches --fast-fail
-./build push --no-block
-./build set_latest
+    set +x
+    echo "Compile and push"
+    ./build compile --branches --fast-fail
+    ./build push --no-block
+    ./build set_latest
 
-./build compile --incoming --fast-fail
-./build push --no-block
+    ./build compile --incoming --fast-fail
+    ./build push --no-block
+popd
 
 echo "DONE"
 
